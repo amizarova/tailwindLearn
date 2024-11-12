674b2fc09443ad2317ea3760905f356ac6dc49f5
Ameliya Sakalouskaya <ameliyasakalouskaya@MacBook-Air-Ameliya.local>
2024-11-12 14:18:20 +0100

    Подключен tailwind корректно

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
 
